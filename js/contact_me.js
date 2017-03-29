c121cd21f55c0a5b6d2ddf213ae8a7e82c94d37e
Quentin Bernard <qbernard18@gmail.com>
2017-03-29 10:29:58 +0200

    test contactme

--- js/contact_me.js
+++ js/contact_me.js
@@ -16,6 +16,7 @@ $(function() {
             var message = $("textarea#message").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
+            console.log(name,email,phone,message);
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
